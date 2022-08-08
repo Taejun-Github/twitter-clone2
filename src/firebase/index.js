c9bec7bda0a8e9b1bc9e61fa2725041c5eef0d60
DESKTOP-MIV89QG\Taejun <tjkimin2021@gmail.com>
2022-08-08 13:26:26 +0900

    authentication

---
+++ src/firebase/index.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDv5SrJrhz52K1VVMOoRG9hpXAjDH6HiCc",
+    authDomain: "mysite-33137.firebaseapp.com",
+    projectId: "mysite-33137",
+    storageBucket: "mysite-33137.appspot.com",
+    messagingSenderId: "279296518500",
+    appId: "1:279296518500:web:7577b17acbc21efa0f7271",
+    measurementId: "G-0QQPRBC0B3"
+  };
+
+  firebase.initializeApp(firebaseConfig)
+
+  export const auth = firebase.auth()
+  export const db = firebase.firestore()
+  export const storage = firebase.storage()
--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -2,9 +2,9 @@
   <div class="flex flex-col items-center">
     <i :class="`fa-brands fa-airbnb text-6xl text-primary p-3 mt-10 ${loading ? 'animate-spin' : ''}`"></i>
     <div class="text-2xl font-bold m-1">내가 만든 사이트 회원가입</div>
-    <input v-model="username" type="text" placeholder="이메일" class="border border-gray-200 m-2 w-1/3 h-8 rounded-sm focus:ring-2 focus:outline-none focus:border-blue-300"/>
-    <input v-model="email" type="text" placeholder="아이디" class="border border-gray-200 m-2 w-1/3 h-8 rounded-sm focus:ring-2 focus:outline-none focus:border-blue-300"/>
-    <input v-model="password" type="text" placeholder="비밀번호" class="border border-gray-200 m-2 w-1/3 h-8 rounded-sm focus:ring-2 focus:outline-none focus:border-blue-300"/>
+    <input v-model="email" type="text" placeholder="이메일" class="border border-gray-200 m-2 w-1/3 h-8 rounded-sm focus:ring-2 focus:outline-none focus:border-blue-300"/>
+    <input v-model="username" type="text" placeholder="아이디" class="border border-gray-200 m-2 w-1/3 h-8 rounded-sm focus:ring-2 focus:outline-none focus:border-blue-300"/>
+    <input v-model="password" type="password" placeholder="비밀번호" class="border border-gray-200 m-2 w-1/3 h-8 rounded-sm focus:ring-2 focus:outline-none focus:border-blue-300"/>
     <button class="bg-blue-600 rounded-3xl text-center w-1/3 h-10 text-white m-2 " @click="onRegister">회원가입</button>
     <router-link to="/login">
     <button class="text-blue-600 text-sm py-2">계정이 이미 있으신가요? 로그인 하기</button>
@@ -14,6 +14,7 @@
 
 <script>
 import {ref} from 'vue'
+import {auth} from '../firebase'
 export default {
   setup() {
     const username = ref('')
@@ -21,8 +22,13 @@ export default {
     const password = ref('')
     const loading = ref(true);
 
-    const onRegister = () => {
-      console.log(username.value, email.value, password.value);
+    const onRegister = async () => {
+      try {
+        const credential = await auth.createUserWithEmailAndPassword(email.value, password.value)
+        console.log(credential);
+      } catch(e) {
+        console.log("create user with email and password error", e);
+      }
     }
 
     return {
